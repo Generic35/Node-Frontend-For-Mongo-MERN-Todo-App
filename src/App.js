965b6955eaeddb2b074a0463766b97253959d8cb
Eric L <ricklamontagne@gmail.com>
2023-06-19 20:16:12 +0300

    (feat) Add the ability to toggle complete

--- src/App.js
+++ src/App.js
@@ -21,6 +21,18 @@ function App() {
       .catch(err => { console.error('>>> error fetching todos: ', err) })
   }
 
+  const completeTodo = async (id) => {
+    const data = await fetch(API_BASE + "/todo/complete/" + id).then(res => res.json())
+
+    setTodos(todos => todos.map((todo) => {
+      if (todo._id === data._id) {
+        todo.complete = data.complete;
+      }
+
+      return todo
+    }))
+  }
+
   return (
     <div className="App">
       <h1>Welcome, Eric</h1>
@@ -28,6 +40,7 @@ function App() {
       <div className="todos">
         {todos.map(todo => {
           return (<div key={todo._id}
+            onClick={() => completeTodo(todo._id)}
             className={"todo " + (todo.complete ? "is-complete" : "")}>
             <div className="checkbox">
               <div className="text">{todo.text}</div>
